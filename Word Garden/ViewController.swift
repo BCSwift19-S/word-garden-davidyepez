a97268a41c50798d8276526e66143ed43421d0f7
David Yepez <yepezd@bc.edu>
2019-02-01 11:42:10 -0500

    3.1 Complete

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -10,10 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userGuessLabel: UILabel!
+    @IBOutlet weak var guessedLetterField: UITextField!
+    @IBOutlet weak var guessLetterButton: UIButton!
+    @IBOutlet weak var guessCountLabel: UILabel!
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet weak var flowerImageView: UIImageView!
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("in viewDidLoad, is guessedLetterField the first responder?" , guessedLetterField.isFirstResponder)
     }
+    
+    
+    func updateUIAfterGuess() {
+        guessedLetterField.resignFirstResponder()
+        guessedLetterField.text = ""
+    }
+    
+    
+    @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
+        print("hey babyyyyyyyy")
+        
+    }
+    
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+        
+        updateUIAfterGuess()        
+        
+    }
+    
+    @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
+        updateUIAfterGuess()
+    }
+    
+    @IBAction func playAgainButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    
+    
 
 
 }
